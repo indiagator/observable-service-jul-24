29967c8ba27bdbe5511716237a55cc75feb3e8a4
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2024-09-03 16:48:04 +0530

    redis commit

--- src/main/java/com/cbt/observableservicejul24/MyController.java
+++ src/main/java/com/cbt/observableservicejul24/MyController.java
@@ -1,16 +1,22 @@
 package com.cbt.observableservicejul24;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 class MyController {
 
     private static final Logger log = LoggerFactory.getLogger(MyController.class);
     private final MyUserService myUserService;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     MyController(MyUserService myUserService) {
         this.myUserService = myUserService;
@@ -27,6 +33,30 @@ class MyController {
         log.info("Got a request");
         return myUserService.getUserId(userId);
     }
+
+    @PostMapping("/save")
+    public String saveObj(@RequestParam("key") String key, @RequestParam("value") String value)
+    {
+        redisTemplate.opsForValue().set(key, value);
+        return "success";
+    }
+
+    @GetMapping("/getip")
+    public ResponseEntity<String> handleRequest(@RequestHeader("X-Forwarded-For") String clientIpAddress, HttpServletRequest request)
+    {
+        if (clientIpAddress == null)
+        {
+            clientIpAddress = request.getRemoteAddr();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Cookie_Session", "etete5646456456");
+
+
+        // Use the clientIpAddress value to identify the client
+        return ResponseEntity.ok().headers(headers).body("Set The Cookie");
+    }
+
 
 
 }
